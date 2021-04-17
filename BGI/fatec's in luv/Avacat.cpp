d355130df7b1122ce46b200248f91fc4354ea435
Gustavo R Lima <rlgustavo1927@gmail.com>
2021-04-17 12:20:54 -0300

    Primeiros passos da Criação do jogo

--- BGI/fatec's in luv/Avacat.cpp
+++ BGI/fatec's in luv/Avacat.cpp
@@ -2,12 +2,14 @@
 #include<graphics.h>
 
 int main()  { 
-  initwindow(1000, 1000, "Fatec's in Luv", 100, 100);
-
-
-
-
-  while(!kbhit());	
-  closegraph();	
-  return 0; 
+	initwindow(600, 500, "Fatec's in Luv", 100, 100);
+	setbkcolor(WHITE);
+	cleardevice;
+	
+	readimagefile("teste.jpg", 100, 100, 300, 300);
+	
+	
+	
+	getch();	
+	closegraph();	
 }
